5c3115ef01bda82e2354a294e239154bea285859
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-07-26 22:19:30 +0300

    change icon

--- src/data/data.js
+++ src/data/data.js
@@ -5,7 +5,7 @@ export const data = [
       category: 'burger',
       image:
         'https://images.unsplash.com/photo-1607013251379-e6eecfffe234?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60',
-      price: '$$$$',
+      price: '5$',
     },
     {
       id: 2,
@@ -13,7 +13,7 @@ export const data = [
       category: 'burger',
       image:
         'https://images.unsplash.com/photo-1553979459-d2229ba7433b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGJ1cmdlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
-      price: '$',
+      price: '4$',
     },
     {
       id: 3,
@@ -21,7 +21,7 @@ export const data = [
       category: 'burger',
       image:
         'https://images.unsplash.com/photo-1608767221051-2b9d18f35a2f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJ1cmdlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
-      price: '$$',
+      price: '2$',
     },
     {
       id: 4,
@@ -29,7 +29,7 @@ export const data = [
       category: 'burger',
       image:
         'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60',
-      price: '$$$',
+      price: '4$',
     },
     {
       id: 5,
@@ -37,7 +37,7 @@ export const data = [
       category: 'pizza',
       image:
         'https://images.unsplash.com/photo-1593560708920-61dd98c46a4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      price: '$$',
+      price: '6$',
     },
     {
       id: 6,
@@ -45,7 +45,7 @@ export const data = [
       category: 'pizza',
       image:
         'https://images.unsplash.com/photo-1604382355076-af4b0eb60143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      price: '$$$',
+      price: '7$',
     },
     {
       id: 7,
@@ -53,7 +53,7 @@ export const data = [
       category: 'pizza',
       image:
         'https://images.unsplash.com/photo-1628840042765-356cda07504e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBpenphfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-      price: '$$$$',
+      price: '10$',
     },
     {
       id: 8,
@@ -61,7 +61,7 @@ export const data = [
       category: 'pizza',
       image:
         'https://images.unsplash.com/photo-1548369937-47519962c11a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2hlZXNlJTIwcGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      price: '$',
+      price: '11$',
     },
     {
       id: 9,
@@ -69,7 +69,7 @@ export const data = [
       category: 'salad',
       image:
         'https://images.unsplash.com/photo-1515543237350-b3eea1ec8082?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      price: '$$',
+      price: '5$',
     },
     {
       id: 10,
@@ -77,7 +77,7 @@ export const data = [
       category: 'salad',
       image:
         'https://images.unsplash.com/photo-1546793665-c74683f339c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      price: '$$$',
+      price: '4$',
     },
     {
       id: 11,
@@ -85,7 +85,7 @@ export const data = [
       category: 'salad',
       image:
         'https://images.unsplash.com/photo-1540420773420-3366772f4999?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2FsYWR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      price: '$$$$',
+      price: '6$',
     },
     {
       id: 12,
@@ -128,7 +128,7 @@ export const data = [
       price: '$$$',
     },
   ];
-  
+
   export const categories = [
     {
       id: 1,
@@ -179,4 +179,3 @@ export const data = [
         'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Placeholder_Plates@3x.png',
     },
   ];
-  
