b0ee1458894fde4033a13e4fbee6565fd0457ee7
Andrei Malets <coolviktori@yandex.by>
2023-04-23 18:27:48 +0300

    create RouterRoot

---
+++ src/routes/RouterRoot.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+export const PATH = {
+  USERS: "/users",
+  HERO: "/",
+  REGISTRATION: "/registration",
+  BLOGGER: "/blogger",
+  POSTS: "/posts",
+  DETAILS: "/detblog",
+  POST_DETAILS: "/postdetails",
+  POST_EDIT: "/postedit",
+  SETTINGS: "/settings",
+};
+const RouterRoot = () => {
+  return <div></div>;
+};
+
+export default RouterRoot;
