f05c2dec27d2c4e56e5b2700f48803f39c52245e
Andrei Malets <coolviktori@yandex.by>
2023-04-23 17:56:07 +0300

    changes Header

--- src/components/Header/Main/HeadlineCards.jsx
+++ src/components/Header/Main/HeadlineCards.jsx
@@ -28,7 +28,7 @@ const HeadlineCards = () => {
           <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
             <p className="font-bold text-2xl px-2 pt-4">{item.title}</p>
             <p className="px-2">{item.text}</p>
-            <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            <button className="border-white bg-white text-black mx-2 absolute bottom-4 hover:bg-orange-500 hover:text-white">
               Order Now
             </button>
           </div>
