017769751b6d4841b9d65d502a495f67da4b1d63
Andrei Malets <coolviktori@yandex.by>
2023-04-23 18:20:53 +0300

    changes Food

--- src/components/FoodList/Food.jsx
+++ src/components/FoodList/Food.jsx
@@ -33,9 +33,7 @@ const Food = () => {
 
   return (
     <div className="max-w-[1640px] m-auto px-4 py-12">
-      <h1 className="text-orange-600 font-bold text-4xl text-center">
-        Top Rated Menu Items
-      </h1>
+      <h1 className="text-orange-600 font-bold text-4xl text-center">Menu</h1>
       {/* Filter Row */}
       <div className="flex flex-col lg:flex-row justify-between">
         {/* Fliter Type */}
