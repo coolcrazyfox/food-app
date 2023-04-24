8d700da244088a93517bd2d1e7fe923fe43eed07
Andrei Malets <coolviktori@yandex.by>
2023-04-24 23:11:46 +0300

    changes Cart

--- src/components/Header/Cart/CartButton.jsx
+++ src/components/Header/Cart/CartButton.jsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsFillCartFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 const CartButton = () => {
+  const [sumPrice, setSumPrice] = useState(100);
   return (
     <Link to="/">
       <button
         onClick={() => alert("cart onClick ")}
         className="bg-black text-white hidden md:flex items-center py-2 rounded-full "
       >
+        <span className="mr-2"> {sumPrice}$ </span>
         <BsFillCartFill
           size={20}
           className="mr-2 text-orange-500 hover:text-white "
         />{" "}
-        Cart
+        {/* Cart */}
         <span className="ml-2 bg-white text-black rounded-full max-h-full w-6">
           0
         </span>
