1d33b2716720bfc1e9e59c005d7bee5e7d1f3259
Andrei Malets <coolviktori@yandex.by>
2023-04-25 20:44:53 +0300

    changes Food

--- src/components/FoodList/FoodCard.jsx
+++ src/components/FoodList/FoodCard.jsx
@@ -15,11 +15,11 @@ const FoodCard = ({ food }) => {
       <div className="flex justify-between px-2 py-4">
         <p className="font-bold">{food.name}</p>
 
-        <p className=" flex flex-row">
+        <p className=" flex flex-row select-none">
           price: &nbsp;{food.price}$&nbsp;
           <div
             onClick={onClickAddItemHandler}
-            className="bg-orange-500 text-white  px-2 rounded-full w-15 cursor-pointer hover:bg-black flex flex-row"
+            className="select-none border-2  border-orange-500 bg-white text-orange-500 px-2 rounded-full w-15 cursor-pointer hover:bg-orange-500 hover:text-white flex flex-row"
           >
             Add
             {addItem > 0 && (
