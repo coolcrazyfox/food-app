b4979f1dc1da4859ba51acf2c07fdb725f460cb0
Andrei Malets <coolviktori@yandex.by>
2023-04-25 22:28:14 +0300

    changes Categories

--- src/components/FoodList/CategoriesFood.jsx
+++ src/components/FoodList/CategoriesFood.jsx
@@ -4,7 +4,7 @@ const CategoriesFood = ({ title, filterTypeHandler }) => {
   return (
     <button
       onClick={filterTypeHandler}
-      className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
+      className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white active:bg-gray-400"
     >
       {title}
     </button>
