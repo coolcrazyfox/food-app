083400dad9889faf1b813ad30ddcaa852716a9c7
Andrei Malets <coolviktori@yandex.by>
2023-04-25 21:20:52 +0300

    changes Header

--- src/components/Header/Main/HeadlineCards.jsx
+++ src/components/Header/Main/HeadlineCards.jsx
@@ -3,19 +3,19 @@ import React from "react";
 const HeadlineCards = () => {
   const headlineCardState = [
     {
-      id: 1,
-      title: "Sun's Out, BOGO's Out",
-      text: "Through 8/26",
+      id: 3,
+      title: "New seasonal MENU",
+      text: "Through 6/21",
       url: "https://images.unsplash.com/photo-1544025162-d76694265947?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJicXxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60",
     },
     {
-      id: 2,
+      id: 1,
       title: "New Restaurants",
       text: "Added Daily",
       url: "https://media-cdn.tripadvisor.com/media/photo-s/1b/30/f5/ee/caption.jpg",
     },
     {
-      id: 3,
+      id: 2,
       title: "We Deliver Desserts Too",
       text: "Tasty Treats",
       url: "https://images.unsplash.com/photo-1559715745-e1b33a271c8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGRlc3NlcnR8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
