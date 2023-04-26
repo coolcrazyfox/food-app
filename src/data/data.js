df9201bd18e6e4f16b03a3a4e707ece933fbcdd4
Andrei Malets <coolviktori@yandex.by>
2023-04-26 22:29:46 +0300

    changes data

--- src/data/data.js
+++ src/data/data.js
@@ -3,7 +3,7 @@ export const data = [
     id: 0,
     imageUrl:
       "https://dodopizza.azureedge.net/static/Img/Products/f035c7f46c0844069722f2bb3ee9f113_584x584.jpeg",
-    name: "Pepperoni Fresh with pepper",
+    name: "Pepperoni & pepper",
     types: [0, 1],
     sizes: [26, 30, 40],
     price: 15,
