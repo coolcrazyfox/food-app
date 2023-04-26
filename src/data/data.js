1020fcfbf10ee8a4ff14f0e15e082ac0f6511f14
Andrei Malets <coolviktori@yandex.by>
2023-04-26 22:09:31 +0300

    changes data

--- src/data/data.js
+++ src/data/data.js
@@ -3,7 +3,7 @@ export const data = [
     id: 0,
     imageUrl:
       "https://dodopizza.azureedge.net/static/Img/Products/f035c7f46c0844069722f2bb3ee9f113_584x584.jpeg",
-    title: "Пепперони Фреш с перцем",
+    name: "Пепперони Фреш с перцем",
     types: [0, 1],
     sizes: [26, 30, 40],
     price: 803,
@@ -14,7 +14,7 @@ export const data = [
     id: 1,
     imageUrl:
       "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
-    title: "Сырная",
+    name: "Сырная",
     types: [0],
     sizes: [26, 40],
     price: 245,
@@ -25,7 +25,7 @@ export const data = [
     id: 2,
     imageUrl:
       "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/6652fec1-04df-49d8-8744-232f1032c44b.jpg",
-    title: "Цыпленок барбекю",
+    name: "Цыпленок барбекю",
     types: [0],
     sizes: [26, 40],
     price: 295,
@@ -36,7 +36,7 @@ export const data = [
     id: 3,
     imageUrl:
       "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/af553bf5-3887-4501-b88e-8f0f55229429.jpg",
-    title: "Кисло-сладкий цыпленок",
+    name: "Кисло-сладкий цыпленок",
     types: [1],
     sizes: [26, 30, 40],
     price: 275,
@@ -47,7 +47,7 @@ export const data = [
     id: 4,
     imageUrl:
       "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg",
-    title: "Чизбургер-пицца",
+    name: "Чизбургер-пицца",
     types: [0, 1],
     sizes: [26, 30, 40],
     price: 415,
@@ -58,7 +58,7 @@ export const data = [
     id: 5,
     imageUrl:
       "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/1e1a6e80-b3ba-4a44-b6b9-beae5b1fbf27.jpg",
-    title: "Крэйзи пепперони",
+    name: "Крэйзи пепперони",
     types: [0],
     sizes: [30, 40],
     price: 580,
@@ -69,7 +69,7 @@ export const data = [
     id: 6,
     imageUrl:
       "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/d2e337e9-e07a-4199-9cc1-501cc44cb8f8.jpg",
-    title: "Пепперони",
+    name: "Пепперони",
     types: [0, 1],
     sizes: [26, 30, 40],
     price: 675,
@@ -80,7 +80,7 @@ export const data = [
     id: 7,
     imageUrl:
       "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/d48003cd-902c-420d-9f28-92d9dc5f73b4.jpg",
-    title: "Маргарита",
+    name: "Маргарита",
     types: [0, 1],
     sizes: [26, 30, 40],
     price: 450,
@@ -91,7 +91,7 @@ export const data = [
     id: 8,
     imageUrl:
       "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/ec29465e-606b-4a04-a03e-da3940d37e0e.jpg",
-    title: "Четыре сезона",
+    name: "Четыре сезона",
     types: [0, 1],
     sizes: [26, 30, 40],
     price: 395,
@@ -102,7 +102,7 @@ export const data = [
     id: 9,
     imageUrl:
       "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/30367198-f3bd-44ed-9314-6f717960da07.jpg",
-    title: "Овощи и грибы 🌱",
+    name: "Овощи и грибы 🌱",
     types: [0, 1],
     sizes: [26, 30, 40],
     price: 285,
