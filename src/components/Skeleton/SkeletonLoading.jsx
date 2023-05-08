2b50e15da7d443f655a1971df8adbc49a7270349
Andrei Malets <coolviktori@yandex.by>
2023-05-08 14:45:37 +0300

    changes Skeleton

--- src/components/Skeleton/SkeletonLoading.jsx
+++ src/components/Skeleton/SkeletonLoading.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const SkeletonLoading = () => {
-  return <div></div>;
+    const=''
+  return (
+    <div className="border shadow-lg rounded-lg hover:scale-105 duration-400">
+        <img
+        src={image}
+        alt={'skeleton'}
+        className="w-full h-[300px] object-cover rounded-t-lg"
+      />
+    </div>
+  );
 };
 
 export default SkeletonLoading;
