3770d5b916e9997340bfa3cc4413128e2bb73f49
Andrei Malets <coolviktori@yandex.by>
2023-05-08 15:24:51 +0300

    changes Skeleton

--- src/components/Skeleton/SkeletonLoading.jsx
+++ src/components/Skeleton/SkeletonLoading.jsx
@@ -9,6 +9,18 @@ const SkeletonLoading = () => {
         alt={"skeleton"}
         className="w-full h-[300px] object-cover rounded-t-lg"
       />
+      <div className="flex justify-between px-2 py-4">
+        <div className="flex flex-row font-bold  ">
+          <div className="px-2 flex flex-row ">
+            <div className="text-orange-500 h-10" />
+            <span className="text-base px-1 h-10"></span>
+          </div>
+        </div>
+
+        <div className=" flex flex-row select-none">
+          <div className="select-none flex flex-row"></div>
+        </div>
+      </div>
     </div>
   );
 };
