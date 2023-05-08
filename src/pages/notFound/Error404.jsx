1e3c921bd605f95bfdd85d1aadd42d0e8c6fd912
Andrei Malets <coolviktori@yandex.by>
2023-05-09 00:28:41 +0300

    changes Error404

--- src/pages/notFound/Error404.jsx
+++ src/pages/notFound/Error404.jsx
@@ -3,14 +3,16 @@ import notFound from "../../assets/404.png";
 const Error404 = () => {
   return (
     <div className="max-w-[1640px] m-auto px-4 py-12">
-      <h1 className="text-center text-lg">Oops!</h1>
-      <div className="flex flex-column object-cover mx-20  bg-gray-500 p-10 justify-center">
+      <h1 className="text-center text-2xl">Oops!</h1>
+      <div className="flex flex-column object-cover mx-20   justify-center">
         <img
           className="flex object-cover mx-20 items-center px-10"
           src={notFound}
         />
-        <h1>PAGE NOT FOUND</h1>
       </div>
+      <h1 className="text-center text-orange-500 font-extrabold text-2xl py-5">
+        PAGE NOT FOUND
+      </h1>
     </div>
   );
 };
