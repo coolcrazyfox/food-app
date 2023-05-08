2d56c3e2e23c49be8f7c04c523d58ad00a5442bb
Andrei Malets <coolviktori@yandex.by>
2023-05-08 15:37:26 +0300

    changes Category

--- src/components/Footer/Category.jsx
+++ src/components/Footer/Category.jsx
@@ -13,7 +13,7 @@ const Category = () => {
         setCategories(arr);
       })
     );
-  });
+  }, []);
   return (
     <div className="max-w-[1640px] m-auto px-4 py-12">
       <h1 className="text-orange-600 font-bold text-4xl text-center">
