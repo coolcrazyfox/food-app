ef56745d470505e35c5441dff40676a02f65f390
Andrei Malets <coolviktori@yandex.by>
2023-05-09 11:46:56 +0300

    changes Navbar

--- src/components/Header/NavBar/BurgerMenu.jsx
+++ src/components/Header/NavBar/BurgerMenu.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { AiFillTag } from "react-icons/ai";
 import { TbTruckDelivery } from "react-icons/tb";
-import { FaUserFriends, FaWallet } from "react-icons/fa";
+import { FaUserFriends } from "react-icons/fa";
 import { MdFavorite, MdHelp } from "react-icons/md";
 import { GoHome } from "react-icons/go";
 import { MdOutlineConnectWithoutContact } from "react-icons/md";
+import { IoFastFoodSharp } from "react-icons/io";
 import { Link } from "react-router-dom";
 
 export const initialState = [
@@ -12,56 +13,48 @@ export const initialState = [
     id: 1,
     link: "/food-app",
     title: "Home",
-    color: "#D73486FF",
     icon: <GoHome size={27} />,
   },
   {
     id: 2,
     link: "/orders",
     title: "Orders",
-    color: "#dbc711",
     icon: <TbTruckDelivery size={27} />,
   },
   {
     id: 3,
-    link: "/wallet",
-    title: "Wallet",
-    color: "#6a99ff",
-    icon: <FaWallet size={27} />,
+    link: "/food",
+    title: "Menubar",
+    icon: <IoFastFoodSharp size={27} />,
   },
   {
     id: 4,
     link: "/favorites",
     title: "Favorites",
-    color: "#dbc711",
     icon: <MdFavorite size={27} />,
   },
   {
     id: 5,
     link: "/promotions",
     title: "Promotions",
-    color: "#ec8434",
     icon: <AiFillTag size={27} />,
   },
   {
     id: 6,
     link: "/invite_friends",
     title: "Invite Friends",
-    color: "#793b92",
     icon: <FaUserFriends size={27} />,
   },
   {
     id: 7,
     link: "/help",
     title: "Help",
-    color: "#8BB92CFF",
     icon: <MdHelp size={27} />,
   },
   {
     id: 8,
     link: "/contact",
     title: "Contact",
-    color: "#8BB92CFF",
     icon: <MdOutlineConnectWithoutContact size={27} />,
   },
 ];
--- src/routes/RouterRoot.jsx
+++ src/routes/RouterRoot.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import App from "../App";
 import Error404 from "../pages/notFound/Error404";
 import CartPage from "../pages/cart/CartPage";
 import Hero from "../pages/hero/Hero";
@@ -9,9 +8,9 @@ import Contact from "../components/Footer/Contact";
 import Category from "../components/Footer/Category";
 
 export const PATH = {
-  ORDERS: "/orders",
   HERO: "/food-app",
-  WALLET: "/wallet",
+  ORDERS: "/orders",
+  FOOD: "/food",
   FAVORITES: "/favorites",
   PROMOTIONS: "/promotions",
   ADD_FRIENDS: "/invite_friends",
@@ -24,14 +23,13 @@ const RouterRoot = () => {
     <div>
       <Routes>
         <Route path={PATH.HERO} element={<Hero />} />
+        <Route path={PATH.FOOD} element={<Food />} />
         <Route path={PATH.ORDERS} element={<CartPage />} />
-        <Route path={PATH.WALLET} element={<Food />} />
         <Route path={PATH.FAVORITES} element={<Category />} />
         {/*<Route path={PATH.PROMOTIONS} element={<PostsPage />} />
         <Route path={PATH.ADD_FRIENDS} element={<DetailsBlog />} />
         <Route path={PATH.HELP} element={<DetailsPost />} />*/}
         <Route path={PATH.CONTACT} element={<Contact />} />
-        <Route path={PATH.FOOD} element={<Food />} />
         <Route path={"*"} element={<Error404 />} />
       </Routes>
     </div>
