c089fd339f2c8e2ecba0f7627a656f0a85407ef4
Andrei Malets <coolviktori@yandex.by>
2023-05-09 14:45:23 +0300

    changes CartPage

--- src/pages/cart/CartPage.jsx
+++ src/pages/cart/CartPage.jsx
@@ -24,48 +24,57 @@ const CartPage = () => {
       rating: 9,
     },
   ];
+  const [isEmpty, setIsEmpty] = React.useState(true);
   return (
     <div className="max-w-[1640px] mx-auto p-4">
-      <h1 className="text-orange-600 font-bold text-4xl text-center my-9">
+      <h1
+        onClick={() => setIsEmpty(!isEmpty)}
+        className="text-orange-600 font-bold text-4xl text-center my-9"
+      >
         Cart
       </h1>
-      <HiShoppingCart size={30} />
-      {state.map((s) => (
-        <div
-          key={s.id}
-          className="flex flex-row border shadow-lg rounded-lg hover:scale-105 duration-400 my-4  max-w-[800px]"
-        >
-          <img
-            src={s.imageUrl}
-            alt={"cart item"}
-            className="w-[100px] h-[90px] object-cover rounded-lg "
-          />
-          <div className="flex flex-col text-lg px-7 my-3 h-auto min-w-[300px] ">
-            <h3 className="text-orange-500 font-bold">{s.name}</h3>
-            <h2>
-              told <span>26cm.</span>
-            </h2>
-          </div>
-          <div className=" flex flex-row justify-between items-center px-5 mx-9 h-auto">
-            <span className="flex items-center justify-center mx-2 font-bold  border border-orange-500 rounded-lg min-w-[30px] text-center">
-              -
-            </span>
-            <div className="font-bold mx-2">1</div>
-            <span className="flex items-center justify-center mx-2 font-bold  border border-orange-500 rounded-lg min-w-[30px] text-center">
-              +
-            </span>
-          </div>
-          <div className="flex items-center justify-center mx-2 font-bold   min-w-[30px] text-center">
-            <p>{s.price}$</p>
-          </div>
-          <RiCloseCircleLine
-            size={24}
-            className="flex items-center justify-center ml-20 mt-3 text-gray-400 hover:text-orange-500 font-bold  text-center"
-          />
-        </div>
-      ))}
-
-      <EmptyCart />
+      {isEmpty ? (
+        <EmptyCart />
+      ) : (
+        <>
+          {" "}
+          <HiShoppingCart size={30} />
+          {state.map((s) => (
+            <div
+              key={s.id}
+              className="flex flex-row border shadow-lg rounded-lg hover:scale-105 duration-400 my-4  max-w-[800px]"
+            >
+              <img
+                src={s.imageUrl}
+                alt={"cart item"}
+                className="w-[100px] h-[90px] object-cover rounded-lg "
+              />
+              <div className="flex flex-col text-lg px-7 my-3 h-auto min-w-[300px] ">
+                <h3 className="text-orange-500 font-bold">{s.name}</h3>
+                <h2>
+                  told <span>26cm.</span>
+                </h2>
+              </div>
+              <div className=" flex flex-row justify-between items-center px-5 mx-9 h-auto">
+                <span className="flex items-center justify-center mx-2 font-bold  border border-orange-500 rounded-lg min-w-[30px] text-center">
+                  -
+                </span>
+                <div className="font-bold mx-2">1</div>
+                <span className="flex items-center justify-center mx-2 font-bold  border border-orange-500 rounded-lg min-w-[30px] text-center">
+                  +
+                </span>
+              </div>
+              <div className="flex items-center justify-center mx-2 font-bold   min-w-[30px] text-center">
+                <p>{s.price}$</p>
+              </div>
+              <RiCloseCircleLine
+                size={24}
+                className="flex items-center justify-center ml-20 mt-3 text-gray-400 hover:text-orange-500 font-bold  text-center"
+              />
+            </div>
+          ))}{" "}
+        </>
+      )}
     </div>
   );
 };
