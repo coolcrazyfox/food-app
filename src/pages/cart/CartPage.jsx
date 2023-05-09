30251c50b82b33851325640cbde01a9bba24c4b1
Andrei Malets <coolviktori@yandex.by>
2023-05-09 13:10:04 +0300

    changes Cart

--- src/pages/cart/CartPage.jsx
+++ src/pages/cart/CartPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-
 import EmptyCart from "../../components/Header/Cart/EmptyCart";
+import { HiShoppingCart } from "react-icons/hi";
 
 const CartPage = () => {
   return (
@@ -8,6 +8,8 @@ const CartPage = () => {
       <h1 className="text-orange-600 font-bold text-4xl text-center my-9">
         Cart
       </h1>
+      <HiShoppingCart size={30} />
+      <div className="border shadow-lg rounded-lg hover:scale-105 duration-400"></div>
       <EmptyCart />
     </div>
   );
