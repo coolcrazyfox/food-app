5d9b985ff3bb1cded38e371c26f185fa81db885c
Andrei Malets <coolviktori@yandex.by>
2023-05-10 23:44:14 +0300

    changes CartPage

--- src/pages/cart/CartPage.jsx
+++ src/pages/cart/CartPage.jsx
@@ -43,7 +43,7 @@ const CartPage = () => {
           {" "}
           <div className="flex flex-row justify-between">
             <HiShoppingCart size={30} />
-            <div className="flex flex-row border border-orange-500 p-2 rounded-full">
+            <div className="flex flex-row border border-orange-500 p-2 rounded-full cursor-pointer hover:shadow-lg hover:bg-gray-500 hover:text-white">
               <span className="text-orange-500 mx-1">
                 <RiDeleteBinFill size={24} />
               </span>
