cfd48eb3397c431ecea147190d0acb0bc3b864b9
Andrei Malets <coolviktori@yandex.by>
2023-05-10 23:11:21 +0300

    changes CartPage

--- src/pages/cart/CartPage.jsx
+++ src/pages/cart/CartPage.jsx
@@ -93,13 +93,13 @@ const CartPage = () => {
       <div className="flex flex-row justify-between">
         <div>
           <h1 className="my-2">
-            All:<span className="font-bold text-orange-500">3 pcs.</span>
+            All:<span className="font-bold text-orange-500 ml-1">3 pcs.</span>
           </h1>
         </div>
         <div>
           <h1 className="my-2">
             Order amount:
-            <span className="font-bold text-orange-500"> 400$</span>
+            <span className="font-bold text-orange-500 ml-1"> 400$</span>
           </h1>
         </div>
       </div>
