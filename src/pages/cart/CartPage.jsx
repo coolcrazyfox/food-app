599e138c54b00d868f184bf37f5d6e7039088a51
Andrei Malets <coolviktori@yandex.by>
2023-05-10 23:23:27 +0300

    changes CartPage

--- src/pages/cart/CartPage.jsx
+++ src/pages/cart/CartPage.jsx
@@ -88,34 +88,35 @@ const CartPage = () => {
                 className="flex items-center justify-center ml-20 mt-3 text-gray-400 hover:text-orange-500 font-bold  text-center"
               />
             </div>
-          ))}{" "}
+          ))}
+          <div className="flex flex-row justify-between">
+            <div>
+              <h1 className="my-2">
+                All:
+                <span className="font-bold text-orange-500 ml-1">3 pcs.</span>
+              </h1>
+            </div>
+            <div>
+              <h1 className="my-2">
+                Order amount:
+                <span className="font-bold text-orange-500 ml-1"> 400$</span>
+              </h1>
+            </div>
+          </div>
+          <div className="flex flex-row justify-between">
+            <Link
+              to="/"
+              className="flex flex-row cursor-pointer border  border-gray-400 hover:bg-black hover:text-white hover:border-orange-500 hover:shadow-lg rounded-full px-10"
+            >
+              <BiArrowBack size={18} className="m-1 hover:text-orange-500" />
+              Back
+            </Link>
+            <div className="cursor-pointer border border-orange-500 rounded-full px-10 hover:shadow-lg hover:bg-orange-500 hover:text-white">
+              Pay now
+            </div>
+          </div>
         </>
       )}
-      <div className="flex flex-row justify-between">
-        <div>
-          <h1 className="my-2">
-            All:<span className="font-bold text-orange-500 ml-1">3 pcs.</span>
-          </h1>
-        </div>
-        <div>
-          <h1 className="my-2">
-            Order amount:
-            <span className="font-bold text-orange-500 ml-1"> 400$</span>
-          </h1>
-        </div>
-      </div>
-      <div className="flex flex-row justify-between">
-        <Link
-          to="/"
-          className="flex flex-row cursor-pointer border  border-gray-400 hover:bg-black hover:text-white hover:border-orange-500 hover:shadow-lg rounded-full px-10"
-        >
-          <BiArrowBack size={18} className="m-1 hover:text-orange-500" />
-          Back
-        </Link>
-        <div className="cursor-pointer border border-orange-500 rounded-full px-10 hover:shadow-lg hover:bg-orange-500 hover:text-white">
-          Pay now
-        </div>
-      </div>
     </div>
   );
 };
