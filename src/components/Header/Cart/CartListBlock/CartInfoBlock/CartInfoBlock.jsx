53c692f40c70ba542f68bb1af5c73be87c5cc98a
Andrei Malets <coolviktori@yandex.by>
2023-05-11 18:12:51 +0300

    changes OrderSelectBtn

--- src/components/Header/Cart/CartListBlock/CartInfoBlock/CartInfoBlock.jsx
+++ src/components/Header/Cart/CartListBlock/CartInfoBlock/CartInfoBlock.jsx
@@ -1,26 +1,12 @@
 import React from "react";
-import { BiArrowBack } from "react-icons/bi";
-import { Link } from "react-router-dom";
-import { PATH } from "../../../../../routes/RouterRoot";
 import OrderCount from "./OrderCount";
+import OrderSelectBtn from "./OrderSelectBtn";
 
 const CartInfoBlock = () => {
   return (
     <>
       <OrderCount />
-
-      <div className="flex flex-row justify-between">
-        <Link
-          to={PATH.HERO}
-          className="flex flex-row cursor-pointer border  border-gray-400 hover:bg-black hover:text-white hover:border-orange-500 hover:shadow-lg rounded-full px-10"
-        >
-          <BiArrowBack size={18} className="m-1 hover:text-orange-500" />
-          Back
-        </Link>
-        <div className="cursor-pointer border border-orange-500 rounded-full px-10 hover:shadow-lg hover:bg-orange-500 hover:text-white">
-          Pay now
-        </div>
-      </div>
+      <OrderSelectBtn />
     </>
   );
 };
---
+++ src/components/Header/Cart/CartListBlock/CartInfoBlock/OrderSelectBtn.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BiArrowBack } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import { PATH } from "../../../../../routes/RouterRoot";
+
+const OrderSelectBtn = () => {
+  return (
+    <div className="flex flex-row justify-between">
+      <Link
+        to={PATH.HERO}
+        className="flex flex-row cursor-pointer border  border-gray-400 hover:bg-black hover:text-white hover:border-orange-500 hover:shadow-lg rounded-full px-10"
+      >
+        <BiArrowBack size={18} className="m-1 hover:text-orange-500" />
+        Back
+      </Link>
+      <div className="cursor-pointer border border-orange-500 rounded-full px-10 hover:shadow-lg hover:bg-orange-500 hover:text-white">
+        Pay now
+      </div>
+    </div>
+  );
+};
+
+export default OrderSelectBtn;
