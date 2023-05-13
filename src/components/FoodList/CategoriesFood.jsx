acd76a7c1abffaccf0fe9b65b623e246306886f9
Andrei Malets <coolviktori@yandex.by>
2023-05-13 15:23:55 +0300

    changes CategoriesFood

--- src/components/FoodList/CategoriesFood.jsx
+++ src/components/FoodList/CategoriesFood.jsx
@@ -13,7 +13,7 @@ const CategoriesFood = ({ filterTypeHandler, selectedType }) => {
   ];
   return (
     <>
-      {titleBtn.map((b, i) => (
+      {titleBtn.map((btn, i) => (
         <button
           key={i}
           onClick={() => filterTypeHandler(i)}
@@ -23,7 +23,7 @@ const CategoriesFood = ({ filterTypeHandler, selectedType }) => {
               : "m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white active:bg-gray-400"
           }
         >
-          {b}
+          {btn}
         </button>
       ))}
     </>
