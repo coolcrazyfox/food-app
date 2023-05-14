577404ed9a97e9034999f5540b8ac81af6021075
Andrei Malets <coolviktori@yandex.by>
2023-05-14 17:56:21 +0300

    changes Search

--- src/components/Header/SearchForm/Search.jsx
+++ src/components/Header/SearchForm/Search.jsx
@@ -6,7 +6,7 @@ const Search = () => {
     <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
       <AiOutlineSearch size={25} />
       <input
-        className="bg-transparent p-2 w-full focus:outline-none"
+        className="bg-transparent p-2 w-[180px] sm:w-[370px] lg:w-[450px] focus:outline-none focus:border-1 focus:border-b-[2px] focus:border-orange-500"
         type="text"
         placeholder="Search foods"
       />
