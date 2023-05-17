5682ce04a23b64c3ce704320283f74d28ff5548c
Andrei Malets <coolviktori@yandex.by>
2023-05-17 22:31:10 +0300

    changes store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import { filterSlice } from "./Slices/filterSlice";
 import { cartSlice } from "./Slices/cartSlice";
 import { foodSlice } from "./Slices/foodSlice";
-// test
 
 export const store = configureStore({
   reducer: { filter: filterSlice, cart: cartSlice, food: foodSlice },
