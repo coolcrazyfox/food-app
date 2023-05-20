94eb7ac4c199ab8b90646b18a856071134ef3c0b
Andrei Malets <coolviktori@yandex.by>
2023-05-21 00:27:46 +0300

    changes Search

--- src/components/Header/SearchForm/Search.jsx
+++ src/components/Header/SearchForm/Search.jsx
@@ -3,10 +3,12 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { MdClear } from "react-icons/md";
 
 const Search = ({ searchValue, setSearchValue }) => {
+  const inputRef = React.useRef();
   return (
     <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
       <AiOutlineSearch size={25} />
       <input
+        ref={inputRef}
         value={searchValue}
         onChange={(e) => setSearchValue(e.target.value)}
         className="bg-transparent p-2 w-[180px] sm:w-[370px] lg:w-[450px] focus:outline-none focus:border-1 focus:border-b-[2px] focus:border-orange-500"
