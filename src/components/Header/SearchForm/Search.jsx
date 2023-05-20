b550a1bdc72327e8db91c624cef20fa5f55e24e9
Andrei Malets <coolviktori@yandex.by>
2023-05-21 00:49:34 +0300

    changes Search

--- src/components/Header/SearchForm/Search.jsx
+++ src/components/Header/SearchForm/Search.jsx
@@ -14,7 +14,7 @@ const Search = ({ searchValue, setSearchValue }) => {
   const updateSearchValue = React.useCallback(
     debounce((string) => {
       setSearchValue(string);
-    }, 500),
+    }, 1000),
     []
   );
   const onChangeInput = (e) => {
