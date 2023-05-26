f6996ba9acd1c8ef48e1281dc8607541c0c6b46c
Andrei Malets <coolviktori@yandex.by>
2023-05-26 23:08:36 +0300

    changes navBarSlice

--- src/redux/store/Slices/navBarSlice.js
+++ src/redux/store/Slices/navBarSlice.js
@@ -1,1 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  categoryId: 0,
+  openMenu: false,
+  // // openSort: false,
+  // sortType: {
+  //   name: "Rating (min)",
+  //   sortProperty: "-rating",
+  // },
+};
