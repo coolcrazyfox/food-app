b300fc914179947d089cf0f84d8b64d2003bbbff
Andrei Malets <coolviktori@yandex.by>
2023-05-27 00:30:02 +0300

    changes ThemeButton

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,19 @@ import React, { useState } from "react";
 import CartButton from "./Cart/CartButton";
 import Search from "./SearchForm/Search";
 import NavBar from "./NavBar/NavBar";
+import { useSelector } from "react-redux";
 
 const Header = ({ searchValue, setSearchValue }) => {
+  const theme = useSelector((state) => state.navbar.changeTheme);
+
   return (
-    <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
+    <div
+      className={
+        theme
+          ? "bg-gray-500 max-w-[1640px] rounded-full mx-auto flex justify-between items-center p-4"
+          : "max-w-[1640px] mx-auto flex justify-between items-center p-4"
+      }
+    >
       {/* Left side */}
       <NavBar />
       {/* Search Input */}
