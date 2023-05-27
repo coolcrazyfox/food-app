b0a8ed79f17157960c3372d24c54d2bf5f85bde0
Andrei Malets <coolviktori@yandex.by>
2023-05-28 01:01:10 +0300

    changes SortFood.jsx

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import RouterRoot from "./routes/RouterRoot";
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 
 function App() {
     const theme = useSelector((state) => state.navbar.changeTheme);
-  // const [searchValue, setSearchValue] = React.useState("");
-  return (
-    <div className={theme?'':'bg-gray-800 min-h-screen'}>
-      <Header
-          // searchValue={searchValue} setSearchValue={setSearchValue}
-      />
-      <RouterRoot
-          // searchValue={searchValue}
-      />
-    </div>
-  );
+
+    return (
+        <div className={theme ? '' : 'bg-gray-800 min-h-screen'}>
+            <Header/>
+            <RouterRoot/>
+        </div>
+    );
 }
 
 export default App;
