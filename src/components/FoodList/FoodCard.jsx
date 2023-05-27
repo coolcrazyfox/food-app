577cb34f85644442cce2a9d73dc9feb2e5257060
Andrei Malets <coolviktori@yandex.by>
2023-05-28 00:46:40 +0300

    changes SortFood.jsx

--- src/components/FoodList/FoodCard.jsx
+++ src/components/FoodList/FoodCard.jsx
@@ -3,33 +3,34 @@ import FoodTypeAndSizeBlock from "./FoodTypeAndSizeBlock";
 import AddFoodItem from "./AddFoodItem";
 import {useSelector} from "react-redux";
 
-const FoodCard = ({ ...food }) => {
-    const theme=useSelector(state=>state.navbar.changeTheme)
-  const [addItem, setAddItem] = React.useState(0);
-  const [sizeAndType, setSizeAndType] = React.useState(false);
+const FoodCard = ({...food}) => {
+    const theme = useSelector(state => state.navbar.changeTheme)
+    const [addItem, setAddItem] = React.useState(0);
+    const [sizeAndType, setSizeAndType] = React.useState(false);
 
-  const onClickAddItemHandler = () => {
-    setAddItem((addItem) => addItem + 1);
-  };
-  return (
-    <div className={theme?"border shadow-lg rounded-lg hover:scale-105 duration-400":"border border-gray-600 shadow-gray-600 shadow-lg rounded-lg hover:scale-105 duration-400"}>
-      <img
-        src={food.imageUrl}
-        alt={food.name}
-        className="w-full h-[300px] object-cover rounded-t-lg"
-      />
+    const onClickAddItemHandler = () => {
+        setAddItem((addItem) => addItem + 1);
+    };
+    return (
+        <div
+            className={theme ? "border shadow-lg rounded-lg hover:scale-105 duration-400" : "border border-gray-600 shadow-gray-600 shadow-lg rounded-lg hover:scale-105 duration-400"}>
+            <img
+                src={food.imageUrl}
+                alt={food.name}
+                className="w-full h-[300px] object-cover rounded-t-lg"
+            />
 
-      <AddFoodItem
-        sizeAndType={sizeAndType}
-        setSizeAndType={setSizeAndType}
-        food={food}
-        addItem={addItem}
-        onClickAddItemHandler={onClickAddItemHandler}
-      />
+            <AddFoodItem
+                sizeAndType={sizeAndType}
+                setSizeAndType={setSizeAndType}
+                food={food}
+                addItem={addItem}
+                onClickAddItemHandler={onClickAddItemHandler}
+            />
 
-      {sizeAndType && <FoodTypeAndSizeBlock food={food} />}
-    </div>
-  );
+            {sizeAndType && <FoodTypeAndSizeBlock food={food}/>}
+        </div>
+    );
 };
 
 export default FoodCard;
