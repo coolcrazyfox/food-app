14aa651d55b8c61bc3da6d409785280a8b73efe5
Andrei Malets <coolviktori@yandex.by>
2023-05-27 21:41:51 +0300

    changes App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import RouterRoot from "./routes/RouterRoot";
+import { useSelector } from "react-redux";
 
 function App() {
+    const theme = useSelector((state) => state.navbar.changeTheme);
   const [searchValue, setSearchValue] = React.useState("");
   return (
-    <div>
+    <div className={theme?'':'bg-gray-500 '}>
       <Header searchValue={searchValue} setSearchValue={setSearchValue} />
       <RouterRoot searchValue={searchValue} />
     </div>
