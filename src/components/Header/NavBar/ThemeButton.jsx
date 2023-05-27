77ea7a9fd82584f402b79f23f9dd85728dd119df
Andrei Malets <coolviktori@yandex.by>
2023-05-27 22:12:50 +0300

    changes NavBar.jsx

--- src/components/Header/NavBar/ThemeButton.jsx
+++ src/components/Header/NavBar/ThemeButton.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setChangeTheme } from "../../../redux/store/Slices/navBarSlice";
-import {BsFillEmojiSunglassesFill} from "react-icons/bs";
+import {BsFillEmojiSunglassesFill, BsMoonStarsFill} from "react-icons/bs";
 
 const ThemeButton = () => {
   const dispatch = useDispatch();
@@ -12,24 +12,25 @@ const ThemeButton = () => {
       onClick={() => dispatch(setChangeTheme(!isClick))}
       className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px] cursor-pointer "
     >
-      <p
+      <div
         className={
           isClick
             ? "text-orange-500 p-2 min-w-4"
             : "bg-black text-white rounded-full p-2 min-w-4 hover:bg-orange-600"
         }
       >
-        <BsFillEmojiSunglassesFill size={19}/>
-      </p>{" "}
-      <p
+        <BsMoonStarsFill size={19}l/>
+
+      </div>{" "}
+      <div
         className={
           !isClick
             ? "p-2"
             : "bg-black text-white rounded-full p-2 min-w-4 hover:bg-orange-600"
         }
       >
-        Light
-      </p>
+        <BsFillEmojiSunglassesFill size={19}/>
+      </div>
     </div>
   );
 };
