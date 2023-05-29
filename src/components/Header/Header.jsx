c00f4cc4766ba48c2ba6097e40bfe6019f469840
Andrei Malets <coolviktori@yandex.by>
2023-05-29 19:31:26 +0300

    changes Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,15 +3,13 @@ import CartButton from "./Cart/CartButton";
 import Search from "./SearchForm/Search";
 import NavBar from "./NavBar/NavBar";
 
-const Header = ({ searchValue, setSearchValue }) => {
+const Header = () => {
   return (
-    <div  className=  "max-w-[1640px] mx-auto flex justify-between items-center p-4" >
+    <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
       {/* Left side */}
       <NavBar />
       {/* Search Input */}
-      <Search
-          // searchValue={searchValue} setSearchValue={setSearchValue}
-      />
+      <Search />
       {/* Cart button */}
       <CartButton />
     </div>
