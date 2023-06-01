da9a43a7f96edfcf8c0f436149e3916a386c5a15
Andrei Malets <coolviktori@yandex.by>
2023-06-01 14:01:18 +0300

    changes

--- src/components/FoodList/SortFood.jsx
+++ src/components/FoodList/SortFood.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setSort, setOpenSort } from "../../redux/store/Slices/filterSlice";
+import { setSort } from "../../redux/store/Slices/filterSlice";
 
 export const sortTitle = [
   { id: 1, name: "Rating (min)", sortProperty: "-rating" },
@@ -15,22 +15,15 @@ const SortFood = React.memo(() => {
   const theme = useSelector((state) => state.navbar.changeTheme);
   const dispatch = useDispatch();
   const sortRef = React.useRef(null);
-  const { openSort, sortType } = useSelector((state) => state.filter);
+  const sort = useSelector((state) => state.filter.sortType);
 
-  // const [openSort, setOpenSort] = React.useState(false);
-  const onChangePriceHandler = (obj, openSort) => {
+  const [openSort, setOpenSort] = React.useState(false);
+  const onChangePriceHandler = (obj) => {
     // filterPriceHandler(obj);
     dispatch(setSort(obj));
-    dispatch(setOpenSort("isClose"));
+    setOpenSort(false);
   };
-  const onClickTest = (openSort) => {
-    if (openSort === "isOpen") {
-      dispatch(setOpenSort("isClose"));
-    }
-    if (openSort === "isClose") {
-      dispatch(setOpenSort("isOpen"));
-    }
-  };
+  console.log("sort", sort.sortProperty);
   return (
     <div>
       <p
@@ -43,14 +36,14 @@ const SortFood = React.memo(() => {
         Sort by:
       </p>
       <div className="flex justify-between max-w-[690px] w-full">
-        {openSort === "isOpen" ? (
+        {openSort ? (
           <>
             {sortTitle.map((obj) => (
               <button
                 key={obj.id}
                 onClick={() => onChangePriceHandler(obj)}
                 className={
-                  sortType.sortProperty === obj.sortProperty
+                  sort.sortProperty === obj.sortProperty
                     ? "m-1 border-orange-600 text-white bg-gray-500 hover:bg-orange-600 hover:text-white"
                     : "m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
                 }
@@ -66,9 +59,9 @@ const SortFood = React.memo(() => {
                 ? "flex my-1 ml-4 border-orange-600 bg-gray-100 text-orange-600 hover:bg-orange-600 hover:text-white"
                 : "flex my-1 ml-4 border-orange-600  text-orange-600 hover:bg-orange-600 hover:text-white"
             }
-            onClick={(e) => onClickTest(e)}
+            onClick={() => setOpenSort(!openSort)}
           >
-            {sortType.name}
+            {sort.name}
           </button>
         )}
       </div>
