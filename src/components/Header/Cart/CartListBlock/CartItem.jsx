98cdab5971a153dd1e59af8c51ad14cb65719ad3
Andrei Malets <coolviktori@yandex.by>
2023-06-03 01:23:40 +0300

    changes CartItem

--- src/components/Header/Cart/CartListBlock/CartItem.jsx
+++ src/components/Header/Cart/CartListBlock/CartItem.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { RiCloseCircleLine } from "react-icons/ri";
 
-const CartItem = ({ ...cart }) => {
+const CartItem = ({ id, imageUrl, name, price, type, count }) => {
   return (
     <div className="flex flex-row border shadow-lg rounded-lg hover:scale-105 duration-400 my-4  max-w-[800px]">
       <img
-        src={cart.imageUrl}
+        src={imageUrl}
         alt={"cart item"}
         className="w-[100px] h-[90px] object-cover rounded-lg "
       />
       <div className="flex flex-col text-lg px-7 my-3 h-auto min-w-[300px] ">
-        <h3 className="text-orange-500 font-bold">{cart.name}</h3>
+        <h3 className="text-orange-500 font-bold">{name}</h3>
         <h2>
-          told <span>26cm.</span>
+          {type} <span>26cm.</span>
         </h2>
       </div>
       <div className=" flex flex-row justify-between items-center px-5 mx-9 h-auto">
@@ -22,7 +22,7 @@ const CartItem = ({ ...cart }) => {
         >
           -
         </span>
-        <div className="font-bold mx-2">1</div>
+        <div className="font-bold mx-2">{count}</div>
         <span
           onClick={() => {}}
           className="flex items-center justify-center mx-2 font-bold  border border-orange-500 rounded-lg min-w-[30px] text-center hover:text-white hover:bg-orange-500 hover:shadow-lg "
@@ -31,7 +31,7 @@ const CartItem = ({ ...cart }) => {
         </span>
       </div>
       <div className="flex items-center justify-center mx-2 font-bold   min-w-[30px] text-center">
-        <p>{cart.price}$</p>
+        <p>{price * count}$</p>
       </div>
       <RiCloseCircleLine
         size={24}
