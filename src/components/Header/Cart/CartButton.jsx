1a16ca0eb3fd361ac1b6f0fe8c4f4aa4c8fd540d
Andrei Malets <coolviktori@yandex.by>
2023-06-02 22:55:55 +0300

    changes CartButton

--- src/components/Header/Cart/CartButton.jsx
+++ src/components/Header/Cart/CartButton.jsx
@@ -1,23 +1,22 @@
 import React, { useState } from "react";
 import { BsFillCartFill } from "react-icons/bs";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const CartButton = () => {
-  const [sumPrice, setSumPrice] = useState(100);
+  const { items, totalPrice } = useSelector((state) => state.cart);
+
   return (
     <Link to="/orders">
-      <button
-        onClick={() => alert("cart onClick ")}
-        className="bg-black text-white hidden md:flex items-center py-2 rounded-full "
-      >
-        <span className="mr-2"> {sumPrice}$ </span>
+      <button className="bg-black text-white hidden md:flex items-center py-2 rounded-full ">
+        <span className="mr-2"> {totalPrice}$ </span>
         <BsFillCartFill
           size={20}
           className="mr-2 text-orange-500 hover:text-white "
         />{" "}
         {/* Cart */}
         <span className="ml-2 bg-white text-black rounded-full max-h-full w-6">
-          0
+          {items}
         </span>
       </button>
     </Link>
