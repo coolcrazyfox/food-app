b301b0aa729c2f40cd52be3494b38cd5336b9b94
Andrei Malets <coolviktori@yandex.by>
2023-06-03 01:01:26 +0300

    changes CartButton

--- src/components/Header/Cart/CartButton.jsx
+++ src/components/Header/Cart/CartButton.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 const CartButton = () => {
   const { items, totalPrice } = useSelector((state) => state.cart);
-
+  const totalCount = items.reduce((sum, item) => sum + item.count, 0);
   return (
     <Link to="/orders">
       <button className="bg-black text-white hidden md:flex items-center py-2 rounded-full ">
@@ -17,7 +17,7 @@ const CartButton = () => {
         {/* Cart */}
         {items.length > 0 ? (
           <span className="ml-2 bg-white text-black rounded-full max-h-full w-6">
-            {items.length}
+            {totalCount}
           </span>
         ) : (
           ""
