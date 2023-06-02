6223d2efe8f47929d13191d3625addc34aad6aba
Andrei Malets <coolviktori@yandex.by>
2023-06-03 00:45:04 +0300

    changes CartPage

--- src/pages/cart/CartPage.jsx
+++ src/pages/cart/CartPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import EmptyCart from "../../components/Header/Cart/EmptyCart";
-
+import { useDispatch, useSelector } from "react-redux";
 import CartList from "./../../components/Header/Cart/CartListBlock/CartList";
 import ClearCartBtn from "./../../components/Header/Cart/CartListBlock/ClearCartBtn";
 
 const CartPage = () => {
+  const dispatch = useDispatch();
+  const cartItem = useSelector((state) =>
+    state.cart.items.find((obj) => obj.id === id)
+  );
   const [isEmpty, setIsEmpty] = React.useState(true);
   return (
     <div className="max-w-[1640px] mx-auto p-4">
