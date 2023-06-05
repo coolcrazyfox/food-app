555d99dd8a17d1261a2f01b9067a32a25d7cc59d
Andrei Malets <coolviktori@yandex.by>
2023-06-05 17:08:22 +0300

    changes CartButton

--- src/components/Header/Cart/CartButton.jsx
+++ src/components/Header/Cart/CartButton.jsx
@@ -8,15 +8,12 @@ const CartButton = () => {
   const totalCount = items.reduce((sum, item) => sum + item.count, 0);
   return (
     <Link to="/orders">
-      <button className="bg-black text-white  md:flex items-center py-2 rounded-full ">
-        <span className="mr-2"> {totalPrice}$ </span>
-        <BsFillCartFill
-          size={20}
-          className="mr-2 text-orange-500 hover:text-white "
-        />{" "}
+      <button className="lg:flex-row md:flex-row flex bg-black text-white   items-center py-2 rounded-full ">
+        <span className="lg:mr-2 sm:m-0 bg-slate-600"> {totalPrice}$ </span>
+        <BsFillCartFill className="lg:mr-2 sm:mx-[2px] text-orange-500 hover:text-white " />{" "}
         {/* Cart */}
         {items.length > 0 ? (
-          <span className="ml-2 bg-white text-black rounded-full max-h-full w-6">
+          <span className=" bg-white text-black rounded-full max-h-full lg:w-6 md:min-w-[20px] sm:min-w-[15px] ml-1 px-[4px]">
             {totalCount}
           </span>
         ) : (
