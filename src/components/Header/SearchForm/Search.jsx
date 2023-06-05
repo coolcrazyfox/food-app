9e217fcdf16b5adbd1b7aee7189c5cc2b3d25b11
Andrei Malets <coolviktori@yandex.by>
2023-06-05 15:37:15 +0300

    changes Search

--- src/components/Header/SearchForm/Search.jsx
+++ src/components/Header/SearchForm/Search.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import debounce from "lodash.debounce";
 import { AiOutlineSearch } from "react-icons/ai";
 import { MdClear } from "react-icons/md";
-import {useDispatch, useSelector} from "react-redux";
-import {setSearchValue} from "../../../redux/store/Slices/filterSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { setSearchValue } from "../../../redux/store/Slices/filterSlice";
 
 const Search = () => {
   const [value, setValue] = React.useState("");
-  const dispatch=useDispatch()
-  const searchValue=useSelector(state=>state.filter.searchValue)
+  const dispatch = useDispatch();
+  const searchValue = useSelector((state) => state.filter.searchValue);
   const inputRef = React.useRef();
   const onClickClearHandler = () => {
     dispatch(setSearchValue(""));
@@ -27,13 +27,13 @@ const Search = () => {
   };
 
   return (
-    <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
+    <div className="flex flex-wrap flex-row bg-gray-200 rounded-full  items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
       <AiOutlineSearch size={25} />
       <input
         ref={inputRef}
         value={value}
         onChange={onChangeInput}
-        className="bg-transparent p-2 w-[180px] sm:w-[370px] lg:w-[450px] focus:outline-none focus:border-1 focus:border-b-[2px] focus:border-orange-500"
+        className="bg-transparent p-2 w-[115px] h-auto sm:w-[370px] lg:w-[450px] focus:outline-none focus:border-1 focus:border-b-[2px] focus:border-orange-500"
         type="text"
         placeholder="Search foods"
       />
