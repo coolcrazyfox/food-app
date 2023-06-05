c85c25e28ebd8cdc92d69e198a962090e9526624
Andrei Malets <coolviktori@yandex.by>
2023-06-05 15:43:24 +0300

    changes Search

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import NavBar from "./NavBar/NavBar";
 
 const Header = () => {
   return (
-    <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
+    <div className="max-w-[1640px] mx-auto flex  justify-between items-center p-4">
       {/* Left side */}
       <NavBar />
       {/* Search Input */}
