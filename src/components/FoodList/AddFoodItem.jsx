46fd689c2310fc046a4592d024acf3802e3c9aaa
Andrei Malets <coolviktori@yandex.by>
2023-06-05 14:41:12 +0300

    changes AddFood

--- src/components/FoodList/AddFoodItem.jsx
+++ src/components/FoodList/AddFoodItem.jsx
@@ -24,35 +24,35 @@ const AddFoodItem = ({
             : "flex justify-between px-2 py-4 text-white"
         }
       >
-        <div className="flex flex-row font-bold  ">
-          {name}
-
-          <div className="px-2 flex flex-row ">
-            <RiStarSmileFill size={24} className="text-orange-500" />
+        <div className="flex flex-col">
+          {/* Title food */}
+          <div className="flex  lg:flex-row font-bold sm:flex-col">{name}</div>
+          <div className="flex flex-row w-full ">
+            <span className="mr-2 font-normal ">{price}$</span>
+            <RiStarSmileFill size={24} className="text-orange-500 ml-1" />
             <span className="text-base px-1">{rating}</span>
-          </div>
-        </div>
-
-        <div className=" flex flex-row select-none">
-          <span className=" text-center pt-1">
-            {types && (
-              <HiOutlineBarsArrowDown
-                onClick={() => setSizeAndType(!sizeAndType)}
-                size={20}
-              />
-            )}
-          </span>
-          <span className="mx-2">{price}$</span>
-          <div
-            onClick={onClickAddItemHandler}
-            className="flex flex-row object-cover select-none border-2  border-orange-500 bg-white text-orange-500 px-2 rounded-full  h-7 cursor-pointer hover:bg-orange-500 hover:text-white "
-          >
-            <span className="flex max-w-max min-w-min">Add</span>
-            {count > 0 && (
-              <span className="ms-3 w-6  text-center rounded-full bg-white text-orange-500">
-                {count}
+            {/* Size and Type */}
+            <div className=" flex flex-row select-none">
+              <span className=" text-center pt-1">
+                {types && (
+                  <HiOutlineBarsArrowDown
+                    onClick={() => setSizeAndType(!sizeAndType)}
+                    size={20}
+                  />
+                )}
               </span>
-            )}
+              <div
+                onClick={onClickAddItemHandler}
+                className="flex flex-row mx-2 object-cover select-none border-2  border-orange-500 bg-white text-orange-500 px-2 rounded-full  h-7 cursor-pointer hover:bg-orange-500 hover:text-white "
+              >
+                <span className="flex max-w-max min-w-min ">Add</span>
+                {count > 0 && (
+                  <span className="ms-2 w-6  text-center rounded-full bg-white text-orange-500">
+                    {count}
+                  </span>
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </div>
