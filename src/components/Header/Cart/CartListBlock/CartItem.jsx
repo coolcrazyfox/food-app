02e3e8d755f289df3b9f40542296229371eeae12
Andrei Malets <coolviktori@yandex.by>
2023-06-09 18:06:10 +0300

    changes CartItem

--- src/components/Header/Cart/CartListBlock/CartItem.jsx
+++ src/components/Header/Cart/CartListBlock/CartItem.jsx
@@ -7,36 +7,38 @@ const CartItem = ({ id, weight, imageUrl, name, price, type, count }) => {
       <img
         src={imageUrl}
         alt={"cart item"}
-        className="w-[100px] h-[90px] object-cover rounded-lg "
+        className="flex flex-wrap w-[100px] h-auto object-cover rounded-lg "
       />
-      <div className="flex flex-col text-lg px-7 my-3 h-auto min-w-[300px] ">
+      <div className="flex flex-col text-lg px-7 my-3 h-auto lg:min-w-[300px] md:min-w-fit ">
         <h3 className="text-orange-500 font-bold">{name}</h3>
         <h2>{type}</h2>
-        <div></div>
+        <div className="text-sm">{weight}g</div>
         <div>26cm.</div>
       </div>
-      <div className=" flex flex-row justify-between items-center px-5 mx-9 h-auto">
-        <span
-          onClick={() => {}}
-          className="flex items-center justify-center mx-2 font-bold  border border-orange-500 rounded-lg min-w-[30px] text-center hover:text-white hover:bg-orange-500  hover:shadow-lg "
-        >
-          -
-        </span>
-        <div className="font-bold mx-2">{count}</div>
-        <span
-          onClick={() => {}}
-          className="flex items-center justify-center mx-2 font-bold  border border-orange-500 rounded-lg min-w-[30px] text-center hover:text-white hover:bg-orange-500 hover:shadow-lg "
-        >
-          +
-        </span>
+      <div className="flex flex-row bg-gray-700 flex-wrap">
+        <div className=" flex flex-row lg:mx-9 md:mx-0 justify-between items-center px-5 h-auto">
+          <span
+            onClick={() => {}}
+            className="flex items-center justify-center lg:mx-2 md:-mx-1 font-bold  border border-orange-500 rounded-lg min-w-[30px] text-center hover:text-white hover:bg-orange-500  hover:shadow-lg "
+          >
+            -
+          </span>
+          <div className="font-bold mx-2 ">{count}</div>
+          <span
+            onClick={() => {}}
+            className="flex items-center justify-center lg:mx-2 md:-mx-1 font-bold  border border-orange-500 rounded-lg min-w-[30px] text-center hover:text-white hover:bg-orange-500 hover:shadow-lg "
+          >
+            +
+          </span>
+        </div>
+        <div className="flex items-center justify-center lg:mx-2 md:mx-1 font-bold   min-w-[30px] text-center">
+          {price * count}$
+        </div>
+        <RiCloseCircleLine
+          size={20}
+          className="flex items-center justify-center lg:ml-20 lg:mt-3 md:ml-1 text-gray-400 hover:text-orange-500 font-bold  text-center"
+        />
       </div>
-      <div className="flex items-center justify-center mx-2 font-bold   min-w-[30px] text-center">
-        <p>{price * count}$</p>
-      </div>
-      <RiCloseCircleLine
-        size={24}
-        className="flex items-center justify-center ml-20 mt-3 text-gray-400 hover:text-orange-500 font-bold  text-center"
-      />
     </div>
   );
 };
