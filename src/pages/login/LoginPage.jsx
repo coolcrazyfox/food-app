84d2162abbc00937a86b7bee08e8a462d676bbe5
Andrei Malets <coolviktori@yandex.by>
2023-06-15 22:47:24 +0300

    changes LoginPage

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return <div className="max-w-[1640px] mx-auto p-4">LoginPage</div>;
 };
 
 export default LoginPage;
