fc63cf99d51542e5f3be611eed4c17b0cc4041bc
Andrei Malets <coolviktori@yandex.by>
2023-06-15 23:05:21 +0300

    changes AddFriends

--- src/pages/add_friends/AddFriendsPage.jsx
+++ src/pages/add_friends/AddFriendsPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const AddFriendsPage = () => {
-  return <div>AddFriendsPage</div>;
+  return (
+    <div className="max-w-[1640px] mx-auto p-4">
+      <h1 className="text-orange-600 font-bold text-4xl text-center my-9">
+        Invite Friends
+      </h1>
+    </div>
+  );
 };
 
 export default AddFriendsPage;
