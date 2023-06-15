e316ed23a966e28433c7584cbda23cb69d906510
Andrei Malets <coolviktori@yandex.by>
2023-06-15 23:24:44 +0300

    changes LoginPage

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const LoginPage = () => {
   const state = [
     { id: 1, type: "text", label: "UserName" },
-    { id: 2, type: "password", label: "Password" },
-    { id: 3, type: "text", label: "Password" },
-    { id: 4, type: "text", label: "Password" },
-    { id: 5, type: "text", label: "Password" },
+    { id: 2, type: "text", label: "Email" },
+    { id: 3, type: "password", label: "Create Password" },
+    { id: 4, type: "password", label: "Password" },
+    { id: 5, type: "submit", label: "Password" },
   ];
   return (
     <div className="max-w-[1640px] mx-auto p-4">
@@ -15,7 +15,12 @@ const LoginPage = () => {
       </h1>
       <form>
         <h2>New Account</h2>
-        <div></div>
+        {state.map((l) => (
+          <div>
+            <input type={l.type} />
+            <label>{l.label}</label>
+          </div>
+        ))}
       </form>
     </div>
   );
