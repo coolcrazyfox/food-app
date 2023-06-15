625c197e180dcb6f3ecf0e85cea5059bc4cbfc04
Andrei Malets <coolviktori@yandex.by>
2023-06-15 23:17:28 +0300

    changes LoginPage

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 
 const LoginPage = () => {
+  const state = [
+    { id: 1, type: "text", label: "UserName" },
+    { id: 2, type: "password", label: "Password" },
+    { id: 3, type: "text", label: "Password" },
+    { id: 4, type: "text", label: "Password" },
+    { id: 5, type: "text", label: "Password" },
+  ];
   return (
     <div className="max-w-[1640px] mx-auto p-4">
       <h1 className="text-orange-600 font-bold text-4xl text-center my-9">
         LoginPage
       </h1>
       <form>
-        <div>New Account</div>
+        <h2>New Account</h2>
+        <div></div>
       </form>
     </div>
   );
