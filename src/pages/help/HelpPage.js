6043798c3369aa3f14543713032ed70b889084a7
Andrei Malets <coolviktori@yandex.by>
2023-06-15 23:03:15 +0300

    changes Help

--- src/pages/help/HelpPage.js
+++ src/pages/help/HelpPage.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const HelpPage = () => {
-  return <div>help</div>;
+  return (
+    <div className="max-w-[1640px] mx-auto p-4">
+      <h1 className="text-orange-600 font-bold text-4xl text-center my-9">
+        HelpPage
+      </h1>
+    </div>
+  );
 };
 
 export default HelpPage;
