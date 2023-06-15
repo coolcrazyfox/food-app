d9510c2852f1cc0c127909a800168ccaf5e39ce9
Andrei Malets <coolviktori@yandex.by>
2023-06-15 23:06:26 +0300

    changes FavoritesPage

--- src/pages/favorites/FavoritesPage.jsx
+++ src/pages/favorites/FavoritesPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const FavoritesPage = () => {
-  return <div>FavoritesPage</div>;
+  return (
+    <div className="max-w-[1640px] mx-auto p-4">
+      <h1 className="text-orange-600 font-bold text-4xl text-center my-9">
+        Favorites
+      </h1>
+    </div>
+  );
 };
 
 export default FavoritesPage;
