f1c9bda573f18c4345d4ff6d2ac527bc5fd6338f
Andrei Malets <coolviktori@yandex.by>
2023-06-20 23:44:05 +0300

    changes ClearCartBtn

--- src/components/Header/Cart/CartListBlock/ClearCartBtn.jsx
+++ src/components/Header/Cart/CartListBlock/ClearCartBtn.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { HiShoppingCart } from "react-icons/hi";
 import { RiDeleteBinFill } from "react-icons/ri";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { clearItems } from "../../../../redux/store/Slices/cartSlice";
 
 const ClearCartBtn = () => {
   const dispatch = useDispatch();
+  const theme = useSelector((state) => state.navbar.changeTheme);
   return (
     <>
       <div
         onClick={() => dispatch(clearItems())}
         className="flex flex-row justify-between"
       >
-        <HiShoppingCart size={30} />
+        <HiShoppingCart
+          size={30}
+          className={theme === "dark" ? "" : "text-white"}
+        />
         <div className="flex flex-row border border-orange-500 p-2 rounded-full cursor-pointer hover:shadow-lg hover:bg-gray-500 hover:text-white">
           <span className="text-orange-400 mx-1">
             <RiDeleteBinFill size={24} />
