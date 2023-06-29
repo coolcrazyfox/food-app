c6351b81e57be083dbc280418ec5bd97e6b6133f
Andrei Malets <coolviktori@yandex.by>
2023-06-29 23:52:34 +0300

    changes OrderSelectBtn

--- src/components/Header/Cart/CartListBlock/CartInfoBlock/OrderSelectBtn.jsx
+++ src/components/Header/Cart/CartListBlock/CartInfoBlock/OrderSelectBtn.jsx
@@ -2,10 +2,19 @@ import React from "react";
 import { BiArrowBack } from "react-icons/bi";
 import { Link } from "react-router-dom";
 import { PATH } from "../../../../../routes/RouterRoot";
+import { useSelector } from "react-redux";
 
 const OrderSelectBtn = () => {
+  const theme = useSelector((state) => state.navbar.changeTheme);
+
   return (
-    <div className="flex flex-row justify-between">
+    <div
+      className={
+        theme === "dark"
+          ? "flex flex-row justify-between"
+          : "flex flex-row justify-between text-white"
+      }
+    >
       <Link
         to={PATH.FOOD}
         className="flex flex-row cursor-pointer border  border-gray-400 hover:bg-black hover:text-white hover:border-orange-500 hover:shadow-lg rounded-full px-10"
