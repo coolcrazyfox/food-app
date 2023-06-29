4b30350dffffd5cd4c6a1010ac4216ed2d6f8282
Andrei Malets <coolviktori@yandex.by>
2023-06-29 23:44:19 +0300

    changes OrderCount

--- src/components/Header/Cart/CartListBlock/CartInfoBlock/OrderCount.jsx
+++ src/components/Header/Cart/CartListBlock/CartInfoBlock/OrderCount.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const OrderCount = () => {
+  const theme = useSelector((state) => state.navbar.changeTheme);
+
   const { items, totalPrice } = useSelector((state) => state.cart);
   const totalCount = items.reduce((sum, item) => sum + item.count, 0);
   return (
     <div className="flex flex-row justify-between">
-      <div>
+      <div className={theme === "dark" ? "" : "text-white"}>
         <h1 className="my-2">
           All:
           <span className="font-bold text-orange-500 ml-1">
@@ -14,7 +16,7 @@ const OrderCount = () => {
           </span>
         </h1>
       </div>
-      <div>
+      <div className={theme === "dark" ? "" : "text-white"}>
         <h1 className="my-2">
           Order amount:
           <span className="font-bold text-orange-500 ml-1">
