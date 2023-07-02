ef830334b3dc0f9ed17caac2e3f3345a40043a86
Andrei Malets <coolviktori@yandex.by>
2023-07-02 23:55:33 +0300

    changes CartPage

--- src/pages/cart/CartPage.jsx
+++ src/pages/cart/CartPage.jsx
@@ -9,7 +9,7 @@ const CartPage = () => {
   return (
     <div className="max-w-[1640px] mx-auto p-4">
       <h1 className="text-orange-600 font-bold text-4xl text-center my-9">
-        Cart
+        Basket
       </h1>
       {items.length > 0 ? (
         <>
