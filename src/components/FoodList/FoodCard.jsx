b5353759c1bb5f9c8e39b3fbd29753395e65bfc0
Andrei Malets <coolviktori@yandex.by>
2023-07-12 23:37:19 +0300

    changes FoodCard

--- src/components/FoodList/FoodCard.jsx
+++ src/components/FoodList/FoodCard.jsx
@@ -66,6 +66,50 @@ const FoodCard = ({
         onClickAddItemHandler={onClickAdd}
         count={addedCount}
       /> */}
+      <div
+        className={
+          theme === "dark"
+            ? "flex justify-between px-2 py-4"
+            : "flex justify-between px-2 py-4 text-white"
+        }
+      >
+        <div className="flex flex-col">
+          {/* Title food */}
+          <div className="flex  lg:flex-row font-bold sm:flex-col ml-2 mb-1">
+            {name}
+          </div>
+          <div className="flex mx-2 mb-1 text-orange-500 font-bold">
+            {weight}g
+          </div>
+          <div className="flex flex-row w-full ">
+            <div className=" flex flex-row select-none  justify-between ">
+              <div
+                onClick={onClickAdd}
+                className="flex flex-row  mx-2 object-cover select-none border-2  border-orange-500 bg-white text-orange-500 px-2 rounded-full  h-7 cursor-pointer hover:bg-orange-500 hover:text-white "
+              >
+                <span className="flex max-w-max min-w-min ">Add</span>
+                {addedCount > 0 && (
+                  <span className="ms-2 w-6  text-center rounded-full bg-white text-orange-500">
+                    {addedCount}
+                  </span>
+                )}
+              </div>
+              <span className="mr-2 font-normal ">{price}$</span>
+              <RiStarSmileFill size={24} className="text-orange-500 ml-1" />
+              <span className="text-base px-1 mr-[2px]">{rating}</span>
+              {/* Size and Type */}
+              <span className=" text-center pt-1">
+                {types && (
+                  <HiOutlineBarsArrowDown
+                    onClick={() => setSizeAndType(!sizeAndType)}
+                    size={20}
+                  />
+                )}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
 
       {sizeAndType && (
         <FoodTypeAndSizeBlock
