c4343aa034209b0717acf2ba4fe9aad355422582
Andrei Malets <coolviktori@yandex.by>
2023-07-30 23:32:35 +0300

    changes SuperModal

--- src/components/Modal/SuperModal.jsx
+++ src/components/Modal/SuperModal.jsx
@@ -8,7 +8,13 @@ const SuperModal = () => {
       <div>
         <button onClick={() => setOpenModal(true)}>Open</button>
       </div>
-      <Dialog open={openModal} onClose={() => setOpenModal(false)}></Dialog>
+      <Dialog open={openModal} onClose={() => setOpenModal(false)}>
+        <div className="w-[300px] bg-slate-500 h-auto">
+          <Dialog.Panel className={"w-[270px] h-auto bg-green-300"}>
+            <Dialog.Title className={"text-white"}>Modal form</Dialog.Title>
+          </Dialog.Panel>
+        </div>
+      </Dialog>
     </div>
   );
 };
