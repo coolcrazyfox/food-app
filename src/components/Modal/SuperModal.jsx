c5d434324b8cd6d5edd1fb9c9c40aa96d8b0a853
Andrei Malets <coolviktori@yandex.by>
2023-07-30 23:20:25 +0300

    create SuperModal.jsx

---
+++ src/components/Modal/SuperModal.jsx
@@ -0,0 +1,15 @@
+import { Dialog } from "@headlessui/react";
+import React from "react";
+
+const SuperModal = () => {
+  return (
+    <div>
+      <div>
+        <button>Open</button>
+      </div>
+      <Dialog></Dialog>
+    </div>
+  );
+};
+
+export default SuperModal;
