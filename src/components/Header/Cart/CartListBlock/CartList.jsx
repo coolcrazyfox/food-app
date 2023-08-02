db45d8453a29c70c3a6bd5235f0b21518aaac52b
Andrei Malets <coolviktori@yandex.by>
2023-08-02 14:53:41 +0300

    changes CartList

--- src/components/Header/Cart/CartListBlock/CartList.jsx
+++ src/components/Header/Cart/CartListBlock/CartList.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import CartItem from "./CartItem";
 import CartInfoBlock from "./CartInfoBlock/CartInfoBlock";
 import { useSelector } from "react-redux";
-import SuperModal from "../../../Modal/SuperModal";
 
 const CartList = () => {
   const items = useSelector((state) => state.cart.items);
-  console.log("cart", items);
   return (
     <>
       {items.map((cart) => (
