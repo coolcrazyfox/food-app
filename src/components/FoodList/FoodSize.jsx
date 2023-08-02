ba55824120a463be766c6a3b4bda1b0e1ee61208
Andrei Malets <coolviktori@yandex.by>
2023-08-03 00:49:41 +0300

    changes FoodSize

--- src/components/FoodList/FoodSize.jsx
+++ src/components/FoodList/FoodSize.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { setActiveSize } from "../../redux/store/Slices/foodSlice";
 
-const FoodSize = ({ sizes, setActiveSize, activeSize }) => {
+const FoodSize = ({ sizes, activeSize }) => {
+  const dispatch = useDispatch();
+
+  const onClickActiveSize = (i) => {
+    dispatch(setActiveSize(i));
+  };
   return (
     <div className="flex justify-between px-2 py-1">
       <ul className="flex flex-row px-1  w-full justify-between ">
         {sizes.map((s, i) => (
           <li
-            onClick={() => setActiveSize(i)}
+            onClick={(i) => dispatch(setActiveSize(i))}
             className={
               activeSize === i
                 ? "text-white mx-1 rounded-md bg-orange-500  w-full text-center cursor-pointer"
