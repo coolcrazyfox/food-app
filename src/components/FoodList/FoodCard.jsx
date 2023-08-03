2e506c7d23ae146666ff2d1db009938225a8fbce
Andrei Malets <coolviktori@yandex.by>
2023-08-03 15:58:30 +0300

    changes AddFoodItem

--- src/components/FoodList/FoodCard.jsx
+++ src/components/FoodList/FoodCard.jsx
@@ -57,60 +57,16 @@ const FoodCard = ({
         className="lg:w-full h-[300px] md:w-screen object-cover rounded-t-lg"
       />
 
-      {/* <AddFoodItem
+      <AddFoodItem
         sizeAndType={sizeAndType}
         setSizeAndType={setSizeAndType}
         name={name}
         price={price}
-        types={types}
         rating={rating}
         weight={weight}
-        onClickAddItemHandler={onClickAdd}
-        count={addedCount}
-      /> */}
-      <div
-        className={
-          theme === "dark"
-            ? "flex justify-between px-2 py-4"
-            : "flex justify-between px-2 py-4 text-white"
-        }
-      >
-        <div className="flex flex-col">
-          {/* Title food */}
-          <div className="flex  lg:flex-row font-bold sm:flex-col ml-2 mb-1">
-            {name}
-          </div>
-          <div className="flex mx-2 mb-1 text-orange-500 font-bold">
-            {weight}g
-          </div>
-          <div className="flex flex-row w-full ">
-            <div className=" flex flex-row select-none  justify-between ">
-              {/* Add button */}
-              <div
-                onClick={onClickAdd}
-                className="flex flex-row  mx-2 object-cover select-none border-2  border-orange-500 bg-white text-orange-500 px-2 rounded-full  h-7 cursor-pointer hover:bg-orange-500 hover:text-white "
-              >
-                <span className="flex max-w-max min-w-min ">Add</span>
-                {addedCount > 0 && (
-                  <span className="ms-2 w-6  text-center rounded-full bg-white text-orange-500">
-                    {addedCount}
-                  </span>
-                )}
-              </div>
-              <span className="mr-2 font-normal ">{price}$</span>
-              <RiStarSmileFill size={24} className="text-orange-500 ml-1" />
-              <span className="text-base px-1 mr-[2px]">{rating}</span>
-              {/* Size and Type */}
-              <span className=" text-center pt-1 px-2 cursor-pointer hover:text-orange-600">
-                <HiOutlineBarsArrowDown
-                  onClick={() => setSizeAndType(!sizeAndType)}
-                  size={20}
-                />
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
+        onClickAdd={onClickAdd}
+        addedCount={addedCount}
+      />
 
       {sizeAndType && (
         <>
