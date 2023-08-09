5c547181a099bc8a85c1daf4fe53142ec50bbbdf
Andrei Malets <coolviktori@yandex.by>
2023-08-09 20:04:20 +0300

    create SuperInput.module.css

--- src/components/Header/SuperInput/SuperInput.jsx
+++ src/components/Header/SuperInput/SuperInput.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+import s from "./SuperInput.module.css";
 const SuperInput = () => {
   return (
-    <div className="inputBox">
+    <div className={s.inputBox}>
       <input type="text" required />
       <label>Wavy Input TextAnimation</label>
     </div>
