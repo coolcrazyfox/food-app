296290e4d0f0dc3d65c45c74f99905b880ad99bb
Andrei Malets <coolviktori@yandex.by>
2023-08-09 20:44:53 +0300

    changes SuperInput

--- src/components/Header/SuperInput/SuperInput.jsx
+++ src/components/Header/SuperInput/SuperInput.jsx
@@ -6,7 +6,9 @@ const SuperInput = () => {
     .split("")
     .map(
       (letters, i) =>
-        `<span style='transition-delay:${i * 30}ms'>${letters}</span>`
+        `<span style='transition-delay:${i * 30}ms;filter:hue-rotate:(${
+          i * 10
+        }deg);'>${letters}</span>`
     )
     .join("");
   return (
