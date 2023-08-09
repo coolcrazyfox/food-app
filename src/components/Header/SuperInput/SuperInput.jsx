7572cb742f8e4177ce683daee2f713f2c4b592da
Andrei Malets <coolviktori@yandex.by>
2023-08-09 20:24:02 +0300

    changes SuperInput

--- src/components/Header/SuperInput/SuperInput.jsx
+++ src/components/Header/SuperInput/SuperInput.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import s from "./SuperInput.module.css";
 const SuperInput = () => {
+  let label = document.querySelector("label");
+  label.innerHTML = label.innerText
+    .split("")
+    .map((letters, i) => `<span>${letters}</span>`)
+    .join("");
   return (
     <div className={s.main_container}>
       <div className={s.inputBox}>
