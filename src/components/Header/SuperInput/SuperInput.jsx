fd6a64619758cfa1ead57e485adf7b4f4604d181
Andrei Malets <coolviktori@yandex.by>
2023-08-09 20:06:42 +0300

    changes SuperInput

--- src/components/Header/SuperInput/SuperInput.jsx
+++ src/components/Header/SuperInput/SuperInput.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import s from "./SuperInput.module.css";
 const SuperInput = () => {
   return (
-    <div className={s.inputBox}>
-      <input type="text" required />
-      <label>Wavy Input TextAnimation</label>
+    <div className={s.main_container}>
+      <div className={s.inputBox}>
+        <input type="text" required />
+        <label>Wavy Input TextAnimation</label>
+      </div>
     </div>
   );
 };
