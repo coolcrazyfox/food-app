203fb21c0ed3a79aa19ded9bb68b1c835ec470c3
Andrei Malets <coolviktori@yandex.by>
2023-08-09 20:40:13 +0300

    changes style

--- src/components/Header/SuperInput/SuperInput.jsx
+++ src/components/Header/SuperInput/SuperInput.jsx
@@ -4,7 +4,10 @@ const SuperInput = () => {
   let label = document.querySelector("label");
   label.innerHTML = label.innerText
     .split("")
-    .map((letters, i) => `<span>${letters}</span>`)
+    .map(
+      (letters, i) =>
+        `<span style='transition-delay:${i * 30}ms'>${letters}</span>`
+    )
     .join("");
   return (
     <div className={s.main_container}>
