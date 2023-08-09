601751238c9d1c07993541834fa4563863f8ff52
Andrei Malets <coolviktori@yandex.by>
2023-08-09 20:01:50 +0300

    create SuperInput.jsx

---
+++ src/components/Header/SuperInput/SuperInput.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SuperInput = () => {
+  return (
+    <div className="inputBox">
+      <input type="text" required />
+      <label>Wavy Input TextAnimation</label>
+    </div>
+  );
+};
+
+export default SuperInput;
